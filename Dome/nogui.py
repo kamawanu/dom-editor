5e634ccb035b4789f57e6e69bce80ee0fba0f095
Thomas Leonard <talex5@gmail.com>
2002-02-26 17:12:15 +0000

    Started to get it working again...
    git-svn-id: http://dom-editor.googlecode.com/svn/trunk@163 ef21e15d-ca94-4315-9c45-0d95b1b2e117

--- Dome/nogui.py
+++ Dome/nogui.py
@@ -1,23 +1,34 @@
 #!/usr/bin/env python
 
+import findrox
+from rox import choices
+
 import sys
 from xml.dom import ext
+from xml.dom.ext.reader import PyExpat
 
 from Model import Model
 from View import View, Done
-
-model = Model('Document')
-view = View(model)
+from Program import Program, load_dome_program
 
 if len(sys.argv) < 2:
-	print "Usage: python nogui.py <code> [DATA]..."
+	print "Usage: python nogui.py <code> [<document>]"
 	sys.exit(0)
 
-code = sys.argv[1]
-model.load_program(code)
+code = choices.load('Dome', 'RootProgram.xml')
+if code:
+	reader = PyExpat.Reader()
+	doc = reader.fromUri(code)
+	root_program = load_dome_program(doc.documentElement)
+else:
+	root_program = Program('Root')
 
-print "XXX: root_program!"
-view.set_exec((model.root_program.start, 'next'))
+model = Model('Document')
+view = View(model, root_program)
+
+code = sys.argv[1]
+
+view.set_exec((view.root_program.start, 'next'))
 try:
 	while 1:
 		view.do_one_step()
