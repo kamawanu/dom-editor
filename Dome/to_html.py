692931eecbc8db8a82faebb6c2b83bee1f09132b
Thomas Leonard <talex5@gmail.com>
2002-11-27 14:36:27 +0000

    Don't accumulate meta headers when exporting HTML.
    git-svn-id: http://dom-editor.googlecode.com/svn/branches/Gtk2@449 ef21e15d-ca94-4315-9c45-0d95b1b2e117

--- Dome/to_html.py
+++ Dome/to_html.py
@@ -9,9 +9,17 @@ from Ft.Xml import InputSource
 doc = implementation.createDocument(None, 'root', None)
 proc = Processor()
 from cStringIO import StringIO
+
+# The HTML writer adds some header fields, so strip any existing ones out or we'll get
+# two lots...
+
 stream = StringIO('''
-<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+<xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform"
+			      xmlns:h="http://www.w3.org/1999/xhtml">
 <xsl:output method="html"/>
+
+  <xsl:template match='/h:html/h:head/h:meta[@name="generator"]' priority='2'/>
+  <xsl:template match='/h:html/h:head/h:meta[@http-equiv="Content-Type"]'/>
 
   <xsl:template match='@*|node()'>
     <xsl:copy>
